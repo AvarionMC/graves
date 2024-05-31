90d87f21693b95b17e92fd60601712f7e549cac5
Steven Van Ingelgem <steven@vaningelgem.be>
2024-05-31 20:00:42 +0200

    Fixing version bug (issue #15)

--- src/main/java/org/avarion/graves/manager/BukkitVersion.java
+++ src/main/java/org/avarion/graves/manager/BukkitVersion.java
@@ -6,7 +6,7 @@ import org.bukkit.Bukkit;
 public class BukkitVersion {
 
     public static String getVersion() {
-        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        return Bukkit.getServer().getBukkitVersion().split("-")[0];
     }
 
 }
