46dee2e7ab570ef1c65825ca6181540c60880c12
Steven Van Ingelgem <steven@vaningelgem.be>
2024-07-15 18:41:22 +0200

    Remove MySQL preliminary support.

--- src/main/java/org/avarion/graves/integration/GriefDefender.java
+++ src/main/java/org/avarion/graves/integration/GriefDefender.java
@@ -3,6 +3,8 @@ package org.avarion.graves.integration;
 import com.griefdefender.api.Registry;
 import com.griefdefender.api.permission.flag.Flag;
 import com.griefdefender.api.registry.CatalogRegistryModule;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 
 import java.util.Optional;
 
@@ -36,5 +38,10 @@ public final class GriefDefender {
                    .name("graves-teleport")
                    .permission("griefdefender.flag.graves.graves-teleport")
                    .build();
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    public boolean canTeleport(Entity entity, Location location) {
+        return true;
     }
 }
