36059fd2e979f4a41162c70bf4d89cc97b7cc613
Steven Van Ingelgem <steven@vaningelgem.be>
2025-01-16 06:34:22 +0100

    Cleanup a bit

--- src/main/java/org/avarion/graves/manager/GraveManager.java
+++ src/main/java/org/avarion/graves/manager/GraveManager.java
@@ -179,45 +179,44 @@ public final class GraveManager {
                 }
             }
 
-            int count = plugin.getConfigInt("particle.count", grave);
-            String extraParam = plugin.getConfigString("particle.extra", grave);
-            boolean isExtraSet = extraParam == null || extraParam.isBlank() || extraParam.equalsIgnoreCase("null");
-            double extra = isExtraSet ? plugin.getConfigDbl("particle.extra", grave) : 0;
             double offsetX = plugin.getConfigDbl("particle.offset.x", grave);
             double offsetY = plugin.getConfigDbl("particle.offset.y", grave);
             double offsetZ = plugin.getConfigDbl("particle.offset.z", grave);
             location = location.clone().add(offsetX + 0.5, offsetY + 0.5, offsetZ + 0.5);
 
-            Object data = null;
-            if (location.getWorld() != null) {
-                switch (particle.name()) {
-                    case "REDSTONE" -> {
-                        int size = plugin.getConfigInt("particle.dust-size", grave);
-                        Color color = ColorUtil.getColor(plugin.getConfigString("particle.dust-color", grave, "RED"));
-                        if (color == null) {
-                            color = Color.RED;
-                        }
-                        data = new Particle.DustOptions(color, size);
-                    }
-                    case "SHRIEK" -> data = 1;
-                }
+            if (location.getWorld() == null) {
+                return;
+            }
 
-                if (isExtraSet) {
-                    if (data != null) {
-                        location.getWorld().spawnParticle(particle, location, count, 0.0, 0.0, 0.0, extra, data);
-                    }
-                    else {
-                        location.getWorld().spawnParticle(particle, location, count, 0.0, 0.0, 0.0, extra);
-                    }
+            Object data = null;
+            if (particle.name().equals("REDSTONE")) {
+                int size = plugin.getConfigInt("particle.dust-size", grave);
+                Color color = ColorUtil.getColor(plugin.getConfigString("particle.dust-color", grave, "RED"));
+                if (color == null) {
+                    color = Color.RED;
                 }
-                else {
-                    if (data != null) {
-                        location.getWorld().spawnParticle(particle, location, count, data);
-                    }
-                    else {
-                        location.getWorld().spawnParticle(particle, location, count);
-                    }
-                }
+                data = new Particle.DustOptions(color, size);
+            }
+            else if (particle.name().equals("SHRIEK")) {
+                data = 1;
+            }
+
+            int count = plugin.getConfigInt("particle.count", grave);
+            String extraParam = plugin.getConfigString("particle.extra", grave);
+            boolean isExtraSet = extraParam == null || extraParam.isBlank() || extraParam.equalsIgnoreCase("null");
+            double extra = isExtraSet ? plugin.getConfigDbl("particle.extra", grave) : 0;
+
+            if (isExtraSet && data != null) {
+                location.getWorld().spawnParticle(particle, location, count, 0.0, 0.0, 0.0, extra, data);
+            }
+            else if (isExtraSet) {
+                location.getWorld().spawnParticle(particle, location, count, 0.0, 0.0, 0.0, extra);
+            }
+            else if (data != null) {
+                location.getWorld().spawnParticle(particle, location, count, data);
+            }
+            else {
+                location.getWorld().spawnParticle(particle, location, count);
             }
         }
     }
