7fa46e5a3dc50ce5ad8760a62b605a1893fe96f3
Steven Van Ingelgem <steven@vaningelgem.be>
2026-05-31 02:08:33 +0200

    chore(deps): update dependency com.sk89q.worldguard:worldguard-bukkit to v7.0.17 (#304)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     compileOnly("me.lokka30:treasury:1.2.0")
     compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.3")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.16")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.17")
     compileOnly("com.palmergames.bukkit.towny:towny:0.103.0.1")
     compileOnly("de.jeff_media:ChestSortAPI:13.0.0-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.12.2")
