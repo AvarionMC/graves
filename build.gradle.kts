690ea660c22caea1307f02fc6a5f6c0d45f34aec
Steven Van Ingelgem <steven@vaningelgem.be>
2026-07-08 02:08:51 +0200

    chore(deps): update dependency net.coreprotect:coreprotect to v24 (#323)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ dependencies {
     compileOnly("de.themoep:minedown:1.7.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:26.1.0")
     implementation("de.tr7zw:item-nbt-api-plugin:2.15.7")
-    compileOnly("net.coreprotect:coreprotect:23.2")
+    compileOnly("net.coreprotect:coreprotect:24.0")
     compileOnly("com.github.Xyness:SimpleClaimSystem:1.13.1")
 }
 
