eb9ce09bfd1e7433e6e6a589930cd138a6197052
Steven Van Ingelgem <steven@vaningelgem.be>
2026-07-12 02:09:29 +0200

    chore(deps): update dependency com.palmergames.bukkit.towny:towny to v0.103.0.7 (#324)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.4")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.17")
-    compileOnly("com.palmergames.bukkit.towny:towny:0.103.0.6")
+    compileOnly("com.palmergames.bukkit.towny:towny:0.103.0.7")
     compileOnly("de.jeff_media:ChestSortAPI:13.0.0-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.12.3")
     compileOnly("com.github.SkriptLang:Skript:2.16.0-pre1")
